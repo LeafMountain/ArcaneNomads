703e38bdd03fe2145029e532d0ab6f5600c2949c
Alexander Lövberg <lovbergalexander@gmail.com>
2018-02-20 19:45:51 +0100

    Backup

---
+++ Assets/Scripts/AI/AIController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class AIController : MonoBehaviour {
+
+	IAIState currentState;
+	public NavMeshAgent agent;
+
+	void Start(){
+		agent = GetComponent<NavMeshAgent>();
+		ChangeState(new AIWander());
+	}
+
+	public void ChangeState(IAIState newState){
+		if(currentState != null){
+			currentState.Exit();
+		}
+		currentState = newState;
+		currentState.Controller = this;
+		currentState.Enter();
+		Debug.Log("Entered: " + currentState);
+	}
+
+	void Update () {
+		currentState.Update();
+	}
+}
