4f22e4aece8fc200b8539147b083c35c3206c204
Alexander Lövberg <lovbergalexander@gmail.com>
2018-04-04 12:35:19 +0200

    Moved shit around

---
+++ Assets/Scripts/Editor/HierarchySetup.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class HierarchySetup {
+
+	[MenuItem("TreeBranch/Setup Hierarchy")]
+    static void SetupHierarchy()
+    {
+		CreateGameObject("Management");
+		CreateGameObject("GUI");
+		CreateGameObject("Cameras");
+		CreateGameObject("Lights");
+		CreateGameObject("World");
+		CreateGameObject("Terrain", "World");
+		CreateGameObject("Props", "World");
+		CreateGameObject("_Dynamic");
+    }
+
+	static void CreateGameObject(string name, string parent = null){
+		if(!GameObject.Find(name)){
+			GameObject go = Object.Instantiate(new GameObject(name));
+
+			if(parent != null){
+				go.transform.SetParent(GameObject.Find(parent).transform);
+			}
+		}
+	}
+}
