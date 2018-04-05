c987f9eb8347ffda4887222adbe15f295336e566
Alexander Lövberg <lovbergalexander@gmail.com>
2018-04-05 12:01:53 +0200

    backup

---
+++ Assets/MouseLook.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour {
+
+	public UnityVector2Event OnMouseLook;
+
+	//Send out the mouse look direction
+}
