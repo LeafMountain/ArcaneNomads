26c38e4011c4a3ffaec80db2e050ce8383b8de37
Stenkrona <axelstenkrona@me.com>
2018-04-27 19:34:02 +0200

    started to work on MusicManger

---
+++ Assets/Scripts/Audio/MusicManagerFiles/DJ.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class DJ {
+
+	private MusicManager mm;
+	private DJ(){}
+	public  DJ(MusicManager mm)
+	{
+		if(mm.debug) Debug.Log("Constructing <DJ>.......");
+		this.mm = mm;
+
+		if(mm.debug) Debug.Log("<DJ> Constructed!");
+	}
+	public void UpdateState()
+	{
+		if(mm.debug) Debug.Log("DJ.UpdateState");
+
+	}
+	public void MixBar()
+	{
+
+	}
+
+}
