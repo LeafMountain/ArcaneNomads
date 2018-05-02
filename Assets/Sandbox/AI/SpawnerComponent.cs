7a0f63b0d8427ab2948451bf4f2a2d47c055f8e0
Alexander Lövberg <lovbergalexander@gmail.com>
2018-05-02 14:35:13 +0200

    Spawner does its own shit now

--- Assets/Sandbox/AI/SpawnerComponent.cs
+++ Assets/Sandbox/AI/SpawnerComponent.cs
@@ -13,7 +13,45 @@ public class SpawnerComponent : MonoBehaviour {
 	public int numberSpawned;
 	public float timeSinceLastSpawn;
 
+	GameObject[] spawnedObjects;
+
+	void Start(){
+		spawnedObjects = new GameObject[amount];
+
+		for (int i = 0; i < amount; i++)
+		{
+			spawnedObjects[i] = Spawn(this);
+		}
+	}
+
+	GameObject Spawn(SpawnerComponent component){
+		Vector2 spawnArea = component.spawnArea;
+
+		Vector3 spawnPosition = RandomPosition(spawnArea);
+
+		Collider[] colliders = Physics.OverlapSphere(spawnPosition, 0);
+
+		for (int i = 0; i < colliders.Length; i++)
+		{
+			spawnPosition = colliders[i].bounds.ClosestPoint(spawnPosition);
+		}
+		
+		GameObject go = Instantiate(component.prefab, component.transform.position + spawnPosition, Quaternion.Euler(new Vector3(0, Random.Range(0, 359), 0)), component.transform);
+		component.numberSpawned++;
+		component.timeSinceLastSpawn = 0;
+
+		return go;
+	}
+
+	Vector3 RandomPosition(Vector2 area){
+		float spawnX = Random.Range(-(area.x / 2), area.x / 2);
+		float spawnY = Random.Range(-(area.y / 2), area.y / 2);		
+
+		return new Vector3(spawnX, 20, spawnY);
+	}
+
 	void OnDrawGizmosSelected(){
+		Gizmos.color = Color.red;
 		Gizmos.DrawWireCube(transform.position, new Vector3(spawnArea.x, 0, spawnArea.y));
 	}
 }
--- Assets/Sandbox/AI/SpawnerSystem.cs
+++ Assets/Sandbox/AI/SpawnerSystem.cs
@@ -4,50 +4,50 @@ using UnityEngine;
 
 public class SpawnerSystem : ComponentSystem<SpawnerComponent> {
 
-	protected override void OnUpdate(){
-		for (int i = 0; i < components.Length; i++)
-		{			
-			if(components[i].numberSpawned < components[i].amount)
-			{
-				components[i].timeSinceLastSpawn += Time.deltaTime;
+	// protected override void OnUpdate(){
+	// 	for (int i = 0; i < components.Length; i++)
+	// 	{			
+	// 		if(components[i].numberSpawned < components[i].amount)
+	// 		{
+	// 			components[i].timeSinceLastSpawn += Time.deltaTime;
 
-				if(components[i].spawnInterval == 0){
-					for (int y = 0; y < components[i].amount; y++)
-					{
-						Spawn(components[i]);
-					}
-				}
-				else if(components[i].timeSinceLastSpawn > components[i].spawnInterval){
-					Spawn(components[i]);
-				}
-			}
-		}
-	}
+	// 			if(components[i].spawnInterval == 0){
+	// 				for (int y = 0; y < components[i].amount; y++)
+	// 				{
+	// 					Spawn(components[i]);
+	// 				}
+	// 			}
+	// 			else if(components[i].timeSinceLastSpawn > components[i].spawnInterval){
+	// 				Spawn(components[i]);
+	// 			}
+	// 		}
+	// 	}
+	// }
 
-	// int failedSpawnPositions;
-	// int maximumFails = 10;
+	// // int failedSpawnPositions;
+	// // int maximumFails = 10;
 
-	void Spawn(SpawnerComponent component){
-		Vector2 spawnArea = component.spawnArea;
+	// void Spawn(SpawnerComponent component){
+	// 	Vector2 spawnArea = component.spawnArea;
 
-		Vector3 spawnPosition = RandomPosition(spawnArea);
+	// 	Vector3 spawnPosition = RandomPosition(spawnArea);
 
-		Collider[] colliders = Physics.OverlapSphere(spawnPosition, 0);
+	// 	Collider[] colliders = Physics.OverlapSphere(spawnPosition, 0);
 
-		for (int i = 0; i < colliders.Length; i++)
-		{
-			spawnPosition = colliders[i].bounds.ClosestPoint(spawnPosition);
-		}
+	// 	for (int i = 0; i < colliders.Length; i++)
+	// 	{
+	// 		spawnPosition = colliders[i].bounds.ClosestPoint(spawnPosition);
+	// 	}
 		
-		Instantiate(component.prefab, component.transform.position + spawnPosition, Quaternion.Euler(new Vector3(0, Random.Range(0, 359), 0)), component.transform);
-		component.numberSpawned++;
-		component.timeSinceLastSpawn = 0;
-	}
+	// 	Instantiate(component.prefab, component.transform.position + spawnPosition, Quaternion.Euler(new Vector3(0, Random.Range(0, 359), 0)), component.transform);
+	// 	component.numberSpawned++;
+	// 	component.timeSinceLastSpawn = 0;
+	// }
 
-	Vector3 RandomPosition(Vector2 area){
-		float spawnX = Random.Range(-(area.x / 2), area.x / 2);
-		float spawnY = Random.Range(-(area.y / 2), area.y / 2);		
+	// Vector3 RandomPosition(Vector2 area){
+	// 	float spawnX = Random.Range(-(area.x / 2), area.x / 2);
+	// 	float spawnY = Random.Range(-(area.y / 2), area.y / 2);		
 
-		return new Vector3(spawnX, 20, spawnY);
-	}
+	// 	return new Vector3(spawnX, 20, spawnY);
+	// }
 }
