bea05e21a672fc8b8635a61f11a9d5a22b3292f5
Alexander Lövberg <lovbergalexander@gmail.com>
2018-05-25 12:07:19 +0200

    Player static class
    I know it's bad.

---
+++ Assets/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	[HideInInspector] public static Player Instance;
+
+	void Start () {
+		if (!Instance) {
+			Instance = this;
+		} else {
+			Destroy (this);
+		}
+	}
+
+	public void ToggleInput (bool enabled) {
+		GetComponent<InputMapper> ().enabled = enabled;
+		GetComponent<MouseLook> ().enabled = enabled;
+	}
+}
