ac56ce14c7067dff5831574b73921da227ac8787
LeafMountain <lovbergalexander@gmail.com>
2019-04-05 15:21:25 +0200

    Sorting working

--- Assets/Editor/AssetAutoStructure/AssetAutoStructure.cs
+++ Assets/Editor/AssetAutoStructure/AssetAutoStructure.cs
@@ -18,25 +18,15 @@ public struct AssetLabel
 
 public class AssetAutoStructure
 {
-    // Template     = Name_Variation_type_label.suffix
-    // PathTemplate = label/name/filename.suffix
-
-    // Example1     = Bob_Evil_mat_char.mat
-    // Example2     = Bob_Evil_tex_char.tga
-    // Example3     = Bob_Evil_pref_char.prefab
-    // Example3     = Bob_Normal_pref_char.prefab
-    // PathExample1 = Character/Bob/Bob_Evil_mat_char.mats
-
     const int TYPE = 0;
     const int LABEL = 1;
-    // const int NAME = 2;
-    // const int VARIATION = 3;
+
+    const string UNSORTEDFOLDER = "_Unsorted";
 
     public static AssetLabel[] Labels;
     public static string[] UnsortedFolders = { "Packages", "Assets/Editor", "Assets/Plugins", "Assets/3rd-Party", "Assets/_Sandbox", "Assets/Scripts", "Assets/Maps" };
 
     public static bool AllowOnlyLabeled = true;
-    public static int FolderLayers = 4;
 
     public static void CreateDefaultLabels()
     {
@@ -53,26 +43,26 @@ public class AssetAutoStructure
             new AssetLabel("Gear", "gear"),
             new AssetLabel("UI", "ui"),
             new AssetLabel("Audio", "sfx"),
-            new AssetLabel("Terrain", "terrain")
+            new AssetLabel("Terrain", "terrain"),
+            new AssetLabel("Core", "core"),
+
         };
     }
 
-    [MenuItem("Tools/Sort Project")]
+    [MenuItem("Tools/Auto Structure/Sort Project")]
     private static void SortAssets()
     {
         string[] assets = FindAssetsNoFolders("");
 
         if (AllowOnlyLabeled)
-            CreateFolderStructure("Assets/_Unsorted");
+            CreateFolderStructure(UNSORTEDFOLDER);
 
-        CreateFolderStructure("Assets/Test/Test");
+        CreateDefaultLabels();
 
-        // CreateDefaultLabels();
+        for (int i = 0; i < assets.Length; i++)
+            SortFile(assets[i]);
 
-        // for (int i = 0; i < assets.Length; i++)
-        //     SortFile(assets[i]);
-
-        // RemoveEmptyFolders("Assets");
+        RemoveEmptyFolders("Assets");
     }
 
     private static void SortFile(string guid)
@@ -82,10 +72,6 @@ public class AssetAutoStructure
 
         // Check if it's a valid file in a valid location
         {
-            // Check if it has an extension
-            // if (!fileNameWithExtension.Contains("."))
-            //     return;
-
             // check if it's inside a Unsorted Folder
             bool unsortedFolder = false;
             for (int j = 0; j < UnsortedFolders.Length; j++)
@@ -99,30 +85,11 @@ public class AssetAutoStructure
             if (unsortedFolder) return;
         }
 
-        // Remove file extension
-        // string fileName = fileNameWithExtension.Split('.')[0];
-        // string[] splitFileName = SplitName(fileName);
-
-        // // Check if file name is in a valid format
-        // if (splitFileName == null || !CheckIfValidFileName(fileName))
-        // {
-        //     AssetDatabase.MoveAsset(filePath, "Assets/_Unsorted/" + fileNameWithExtension);
-        //     return;
-        // }
-
-        // string label = splitFileName[LABEL];
-        // label = GetFullLabelName(label);
-        // int numberOfLayers = FolderLayers < splitFileName.Length ? FolderLayers : splitFileName.Length - 1;
-
-        // for (int i = LABEL + 1; i < LABEL + numberOfLayers; i++)
-        // {
-        //     label += "/" + splitFileName[i];
-        // }
-
-        string newPath = "Assets/" + ConstructFilePath(guid);
+        string newPath = ConstructFilePath(guid);
         CreateFolderStructure(newPath);
-        // Debug.Log(newPath + "/" + fileNameWithExtension);
-        Debug.Log(AssetDatabase.MoveAsset(filePath, newPath + "/" + fileNameWithExtension));
+        string newCompletePath = "Assets/" + newPath + "/" + fileNameWithExtension;
+        if(filePath != newCompletePath)
+            AssetDatabase.MoveAsset(filePath, newCompletePath);
     }
 
     private static string ConstructFilePath(string guid)
@@ -133,7 +100,7 @@ public class AssetAutoStructure
         string[] tags = GetTags(guid);
 
         if(tags.Length < 2)
-            return "Assets/_Unsorted";
+            return UNSORTEDFOLDER;
 
         // Check variations and compare to the rest of the project
 
@@ -141,23 +108,25 @@ public class AssetAutoStructure
         string currentFileName = tags[1];
         // Start with the first tag
         string finalPath = GetFullLabelName(tags[1]);
+        int previousMatchingAssetsCount = FindAssetsNoFolders(currentFileName).Length;
 
-        // int previousMatchingAssetsCount = FindAssetsNoFolders(finalPath).Length;
-
-        for (int i = 2; i < tags.Length - 1; i++)
+        for (int i = 2; i < tags.Length; i++)
         {
             // Add the new tag
             currentFileName += "_" + tags[i];
+
             // Get all the assets with matching tags
-            string[] matchingAssets = FindAssetsNoFolders(currentFileName);
-            // Get all the assets with matching tags plus the next tag
-            string[] nextLayer = FindAssetsNoFolders(currentFileName + "_" + tags[i + 1]);
+            string[] currentLayer = FindAssetsNoFolders(currentFileName);
+
+            if(currentLayer.Length == 1)
+                break;
 
             // If the next layer has fewer hits, create a folder to gather the variations under
-            if (matchingAssets.Length > nextLayer.Length)
+            if (previousMatchingAssetsCount > currentLayer.Length)
             {
                 // previousMatchingAssetsCount = matchingAssets.Length;
                 finalPath += "/" + tags[i];
+                previousMatchingAssetsCount = currentLayer.Length;
             }
         }
 
@@ -179,27 +148,7 @@ public class AssetAutoStructure
                 return assetLabel.FullLabel;
         }
 
-        return AllowOnlyLabeled ? "Unsorted" : label;
-    }
-
-    private static bool CheckIfValidFileName(string fileName)
-    {
-        // Remove file extension
-        fileName = fileName.Split('.')[0];
-        // Split into the different parts
-        string[] splitFileName = SplitName(fileName);
-
-        // Check if the length is correct
-        if (splitFileName.Length < 2)
-        {
-            return false;
-        }
-
-        // Check if the label exists
-        if (GetFullLabelName(splitFileName[LABEL]) == "Unsorted")
-            return false;
-
-        return true;
+        return AllowOnlyLabeled ? UNSORTEDFOLDER : label;
     }
 
     private static string[] SplitName(string name)
@@ -226,11 +175,9 @@ public class AssetAutoStructure
         string[] assets = AssetDatabase.FindAssets(filter, new[] { "Assets" });
         List<string> filesOnly = new List<string>();
 
-
         for (int i = assets.Length - 1; i >= 0; i--)
         {
             bool isFolder = AssetDatabase.IsValidFolder(AssetDatabase.GUIDToAssetPath(assets[i]));
-            // Debug.Log(isFolder);
 
             if (!isFolder)
                 filesOnly.Add(assets[i]);
@@ -250,24 +197,24 @@ public class AssetAutoStructure
     private static void CreateFolderStructure(string path)
     {
         string[] folders = path.Split('/');
-        string currentPath = "";
+        string currentPath = "Assets";
 
         for (int i = 0; i < folders.Length; i++)
         {
             string folder = folders[i];
+
             if (!AssetDatabase.IsValidFolder(currentPath + "/" + folder))
             {
-                // string fixedFolder = char.ToUpper(folder[0]) + folder.Substring(1);
-                string fixedFolder = folder;
+                // Make first letter uppercase
+                string fixedFolder = char.ToUpper(folder[0]) + folder.Substring(1);
                 AssetDatabase.CreateFolder(currentPath, fixedFolder);
-                // Debug.Log("Creating folder at " + currentPath + "/" + fixedFolder);
             }
 
-            currentPath += folder + (i != folders.Length - 1 ? "/" : "");
+            currentPath += "/" + folder;
         }
     }
 
-    [MenuItem("Tools/Remove Empty Folders")]
+    [MenuItem("Tools/Auto Structure/Remove Empty Folders")]
     private static void RemoveEmptyFoldersEditorShortcut()
     {
         RemoveEmptyFolders("Assets");
@@ -295,40 +242,5 @@ public class AssetAutoStructure
         }
 
         return false;
-    }
-
-    [MenuItem("Tools/Temp")]
-    static void Temp()
-    {
-        string[] guids = AssetDatabase.FindAssets("env_sand", new[] { "Assets" });
-        foreach (var guid in guids)
-        {
-            string oldPath = AssetDatabase.GUIDToAssetPath(guid);
-            string[] oldPathSplit = oldPath.Split('/');
-            string newPath = "";
-            for (int i = 0; i < oldPathSplit.Length - 1; i++)
-            {
-                newPath += oldPathSplit[i] + "/";
-            }
-
-            string temp = oldPathSplit[oldPathSplit.Length - 1];
-            string[] temp2 = temp.Split('_');
-
-            newPath += temp2[0];
-            newPath += "_env_nature";
-
-            for (int i = 2; i < temp2.Length; i++)
-            {
-                newPath += "_";
-                // if (temp2[i].Contains("building"))
-                // {
-                //     newPath += "city_";
-                // }
-                newPath += temp2[i];
-            }
-
-            // Debug.Log(oldPath + " to " + newPath);
-            AssetDatabase.MoveAsset(oldPath, newPath);
-        }
     }
 }
