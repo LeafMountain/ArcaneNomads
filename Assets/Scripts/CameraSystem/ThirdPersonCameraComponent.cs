a9d95680e601dd51218577746ee23287ce42f410
LeafMountain <lovbergalexander@gmail.com>
2019-04-17 11:48:51 +0200

    Move animation, Camera
    - Move component send proper values to animator now
    - MoveComponent has an option to move relative to the camera
    - MoveComponent has an option to ignore the Y value (move along the ground)
    - Camera follow target, have an offset and a distance (more comming)

---
+++ Assets/Scripts/CameraSystem/ThirdPersonCameraComponent.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCameraComponent : MonoBehaviour
+{
+    public Transform Target;
+    public Vector3 Offset;
+    public float Distance;
+    [Range(0, 1)]
+    public float Smoothing = .3f;
+
+    void Start()
+    {
+
+    }
+
+    void LateUpdate()
+    {
+        Vector3 targetPosition = Target.position;
+        transform.position = targetPosition + Offset + (-transform.forward * Distance);
+    }
+
+    public void AddRotation(Quaternion rotation)
+    {
+        
+    }
+
+    public void SetRotation(Quaternion rotation)
+    {
+
+    }
+}
--- Assets/Scripts/MoveSystem/MoveComponent.cs
+++ Assets/Scripts/MoveSystem/MoveComponent.cs
@@ -9,6 +9,10 @@ public class MoveComponent : MonoBehaviour
     public float MoveSpeed = 1;
     [Range(0, 1)]
     public float Smoothing = .3f;
+    [Range(0, 1)]
+    public float AnimationSmoothing = 0;
+    public bool RelativeToCamera;
+    public bool IgnoreY;
 
     CharacterController characterController;
     Animator animator;
@@ -23,16 +27,32 @@ public class MoveComponent : MonoBehaviour
 
     void Update()
     {
-        Vector3 smoothMove = Vector3.SmoothDamp(transform.position, transform.position + moveVector, ref currentVelocity, Smoothing);
+        Vector3 moveDirection = moveVector;
+        if(RelativeToCamera) moveDirection = Camera.main.transform.TransformVector(moveDirection);
+        if(IgnoreY) moveDirection.y = 0;
+        Vector3 smoothMove = Vector3.SmoothDamp(transform.position, transform.position + moveDirection, ref currentVelocity, Smoothing, MoveSpeed, Time.deltaTime);
         smoothMove += Physics.gravity;
         characterController.Move(smoothMove - transform.position);
+
+        RotateTowardsMoveDir(moveDirection);
+
+        Vector3 localVelocity = transform.InverseTransformDirection(currentVelocity);
+        Debug.DrawRay(transform.position, localVelocity, Color.red);
+        Debug.DrawRay(transform.position, currentVelocity, Color.blue);
         
         // Send velocity to animator if it exists
         if(animator){
-            animator.SetFloat("VelocityX", currentVelocity.x, Smoothing, Time.deltaTime);
-            animator.SetFloat("VelocityY", currentVelocity.y, Smoothing, Time.deltaTime);
-            animator.SetFloat("VelocityZ", currentVelocity.z, Smoothing, Time.deltaTime);
+            animator.SetFloat("VelocityX", localVelocity.x, Smoothing, Time.deltaTime);
+            animator.SetFloat("VelocityY", localVelocity.y, Smoothing, Time.deltaTime);
+            animator.SetFloat("VelocityZ", localVelocity.z, Smoothing, Time.deltaTime);
         }
+    }
+
+    // Temporary
+    void RotateTowardsMoveDir(Vector3 direction)
+    {
+        direction.y = 0;
+        transform.LookAt(transform.position + direction);
     }
 
     public void Move(Vector2 direction)
