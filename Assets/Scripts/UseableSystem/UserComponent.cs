ed031d37a15730bf5aa3c93e77fde885e4c79f15
LeafMountain <lovbergalexander@gmail.com>
2019-04-22 21:21:37 +0200

    Backup of nothing

---
+++ Assets/Scripts/UseableSystem/UserComponent.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class UserComponent : MonoBehaviour
+{
+    public UseableComponent useable;
+    public Transform attachPoint;
+
+    public void Use()
+    {
+        if(useable)
+            useable.Use(this);
+    }
+
+    public void SetUseable(UseableComponent useable)
+    {
+        this.useable = useable;
+        useable.transform.parent = attachPoint;
+        useable.transform.rotation = attachPoint.rotation;
+        useable.transform.position = attachPoint.position;
+    }
+}
