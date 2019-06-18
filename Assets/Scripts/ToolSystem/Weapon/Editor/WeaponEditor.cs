2bc93ef80a83dcb60c35f5132fa359038a2e0923
Alexander Lövberg <lovbergalexander@gmail.com>
2019-06-18 15:05:56 +0200

    Backup before pc explodes (and some gun stats

---
+++ Assets/Scripts/ToolSystem/Weapon/Editor/WeaponEditor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Weapon))]
+public class WeaponEditor : Editor
+{
+    private void OnSceneGUI()
+    {
+        Weapon weapon = (Weapon) target;
+        Transform transform = weapon.transform;
+        
+        if(Event.current.type == EventType.Repaint)
+        {
+            Handles.color = Color.blue;
+            Handles.ConeHandleCap(0, transform.position, transform.rotation, weapon.bulletSpeed, EventType.Repaint);
+        }
+        
+        Handles.RadiusHandle(transform.rotation, transform.position, weapon.bulletSpeed, false);
+        
+        // Debug.Log("Hej");
+    }
+}
--- Assets/Scripts/ToolSystem/Weapon/Weapon.cs
+++ Assets/Scripts/ToolSystem/Weapon/Weapon.cs
@@ -1,17 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Events;
 
 [DisallowMultipleComponent]
 public class Weapon : MonoBehaviour
 {
+    [Header("Gun")]
     public float firerate = .5f;
-    public float bulletSpeed = 10;
+    public int numberOfBullets = 1;
+    [Tooltip("Distance before bullet is destroyed")] public float range = 10;
+
+    [Header("Bullet")]
+    [Tooltip("m/s")] public float bulletSpeed = 10;
+    [Tooltip("In degrees")] public float bulletSpread = 0;
+    public int bulletDamage = 1;
+
+    [Header("Other")]
     public GameObject bulletPrefab;
     public Transform shootOrigin;
-    public int damage;
-
     public UnityEvent OnFire;
 
     bool onCooldown;
@@ -37,12 +45,25 @@ public class Weapon : MonoBehaviour
     {
         onCooldown = true;
 
-        Bullet bullet = Instantiate(bulletPrefab, shootOrigin.position, shootOrigin.rotation).GetComponent<Bullet>();
-        Collider bulletCollider = bullet.GetComponent<Collider>();
-        bullet.Init(damage, bulletSpeed, shootOrigin.forward, this);
+        for (int i = 0; i < numberOfBullets; i++)
+        {
+            Bullet bullet = Instantiate(bulletPrefab, shootOrigin.position, shootOrigin.rotation).GetComponent<Bullet>();
+            Collider bulletCollider = bullet.GetComponent<Collider>();
+            Vector3 bulletDirection = shootOrigin.forward;
+            // bulletDirection =
+            bullet.Init(bulletDamage, bulletSpeed, bulletDirection, this);  
+            Destroy(bullet.gameObject, range / bulletSpeed);  
+        }
 
         OnFire.Invoke();
         yield return new WaitForSeconds(firerate / 1);
         onCooldown = false;
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Vector3 coneDirection = shootOrigin.forward * range;
+        Gizmos.DrawRay(transform.position, coneDirection);
+        Handles.DrawCone(0, shootOrigin.position, shootOrigin.rotation, bulletSpeed);
     }
 }
